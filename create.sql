c36b5021481cf71e6bff1f414adf7c5d8abbc69d
blue <336767@niuitmo.ru>
2023-11-22 18:32:41 +0300

    without procedure and index

--- create.sql
+++ create.sql
@@ -1,154 +1,96 @@
-CREATE TABLE "role" (
-                        "role_id" integer PRIMARY KEY,
-                        "role_name" varchar(20) UNIQUE NOT NULL,
-                        "created_at" timestamp DEFAULT 'now()',
-                        "updated_at" timestamp DEFAULT 'now()'
+CREATE TYPE role AS ENUM ('user', 'boss', 'arendator', 'helper');
+
+
+CREATE TABLE "fraction" (
+    "fraction_id" SERIAL PRIMARY KEY,
+    "fraction_name" varchar(20) UNIQUE NOT NULL,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
 );
 
-CREATE TABLE "reports" (
-                           "report_id" integer PRIMARY KEY,
-                           "report_created_by" integer,
-                           "report_title" varchar(20) NOT NULL,
-                           "report_text" text,
-                           "created_at" timestamp DEFAULT 'now()',
-                           "updated_at" timestamp DEFAULT 'now()'
+CREATE TABLE "person" (
+    "person_id" SERIAL PRIMARY KEY,
+    "user_name" varchar(20) NOT NULL,
+    "person_role_id" role DEFAULT 'user',
+    "person_fraction_id" integer REFERENCES "fraction" ("fraction_id")on delete set null on update cascade,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
 );
+
+CREATE TABLE "report" (
+    "report_id" SERIAL PRIMARY KEY,
+    "report_created_by" integer REFERENCES "person" ("person_id") on delete set null on update cascade,
+    "report_title" varchar(20) NOT NULL,
+    "report_text" text,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
+);
+
+CREATE TYPE status AS ENUM ('created', 'in_process', 'need_check', 'checked', 'answer', 'finished');
 
 CREATE TABLE "task" (
-                        "task_id" integer PRIMARY KEY,
-                        "task_user_to" integer,
-                        "task_user_from" integer,
-                        "task_test" text,
-                        "task_status" status,
-                        "created_at" timestamp,
-                        "updated_at" timestamp,
-                        "task_title" varchar(120)
+    "task_id" SERIAL PRIMARY KEY,
+    "task_person_to" integer REFERENCES "person" ("person_id") on delete set null on update cascade,
+    "task_person_from" integer REFERENCES "person" ("person_id") on delete set null on update cascade,
+    "task_test" text,
+    "task_status" status DEFAULT 'created',
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()',
+    "task_title" varchar(120)
 );
 
 CREATE TABLE "test_result" (
-                               "test_result_id" integer PRIMARY KEY,
-                               "user_id" integer UNIQUE,
-                               "fraction_id" integer,
-                               "created_at" timestamp
-);
-
-CREATE TABLE "user" (
-                        "user_id" integer PRIMARY KEY,
-                        "user_name" varchar(20) NOT NULL,
-                        "user_role_id" integer,
-                        "user_fraction_id" integer,
-                        "created_at" timestamp DEFAULT 'now()',
-                        "updated_at" timestamp DEFAULT 'now()'
-);
-
-CREATE TABLE "event_user" (
-                              "event_participant_id" integer PRIMARY KEY,
-                              "participation_id" integer,
-                              "event_id" integer,
-                              "created_at" timestamp,
-                              "updated_at" timestamp
+    "test_result_id" SERIAL PRIMARY KEY,
+    "person_id" integer UNIQUE REFERENCES "person" ("person_id") on delete set null on update cascade,
+    "fraction_id" integer REFERENCES "fraction" ("fraction_id") on delete set null on update cascade,
+    "created_at" timestamp DEFAULT 'now()'
 );
 
 CREATE TABLE "event" (
-                         "event_id" integer PRIMARY KEY,
-                         "user_name" varchar(20) NOT NULL,
-                         "created_at" timestamp,
-                         "updated_at" timestamp
+     "event_id" SERIAL PRIMARY KEY,
+     "event_name" varchar(20) NOT NULL,
+     "created_at" timestamp DEFAULT 'now()',
+     "updated_at" timestamp DEFAULT 'now()'
 );
 
-CREATE TABLE "fraction" (
-                            "fraction_id" integer PRIMARY KEY,
-                            "fraction_name" varchar(20) UNIQUE NOT NULL,
-                            "created_at" timestamp,
-                            "updated_at" timestamp
-);
-CREATE TYPE status AS ENUM ('created', 'in_process', 'need_check','checked','answer', 'finished');
--- CREATE TABLE "status" (
---                           "status_id" integer PRIMARY KEY,
---                           "status_name" varchar(20) UNIQUE NOT NULL,
---                           "created_at" timestamp,
---                           "updated_at" timestamp
--- );
-
-CREATE TABLE "places_arendator" (
-                                    "id" integer PRIMARY KEY,
-                                    "place_id" integer,
-                                    "arendator_id" integer,
-                                    "from" timestamp,
-                                    "to" timestamp,
-                                    "created_at" timestamp,
-                                    "updated_at" timestamp
+CREATE TABLE "event_user" (
+    "event_participant_id" SERIAL PRIMARY KEY,
+    "participant_id" integer REFERENCES "person" ("person_id") on delete set null on update cascade,
+    "event_id" integer REFERENCES "event" ("event_id") on delete set null on update cascade,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
 );
 
+CREATE TABLE "place" (
+    "place_id" SERIAL PRIMARY KEY,
+    "fraction_id" integer REFERENCES "fraction" ("fraction_id") on delete set null on update cascade,
+    "place_name" varchar(20) UNIQUE,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
+);
+
+CREATE TABLE "place_arendator" (
+    "place_arendator_id" SERIAL PRIMARY KEY,
+    "place_id" integer  REFERENCES "place" ("place_id") on delete set null on update cascade,
+    "arendator_id" integer  REFERENCES "person" ("person_id") on delete set null on update cascade
+);
 CREATE TABLE "characteristic" (
-                                  "charectiristic_id" integer PRIMARY KEY,
-                                  "charectiristic_name" varchar(20) UNIQUE NOT NULL,
-                                  "created_at" timestamp,
-                                  "updated_at" timestamp
+    "charectiristic_id" SERIAL PRIMARY KEY,
+    "charectiristic_name" varchar(20) UNIQUE NOT NULL,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
 );
-
-CREATE TABLE "places" (
-                          "places_id" integer PRIMARY KEY,
-                          "fraction_id" integer,
-                          "places_name" varchar(20) UNIQUE,
-                          "status" status_place,
-                          "created_at" timestamp,
-                          "updated_at" timestamp
+CREATE TABLE "place_characteristic" (
+    "place_characteristic_id" SERIAL PRIMARY KEY,
+    "characteristic_id" integer REFERENCES "characteristic" ("charectiristic_id") on delete set null on update cascade,
+    "place_id" integer REFERENCES "place" ("place_id") on delete set null on update cascade,
+    "value" varchar(20) UNIQUE
 );
-
-CREATE TABLE "places_characteristic" (
-                                         "places_characteristic_id" integer PRIMARY KEY,
-                                         "characteristic_id" integer,
-                                         "place_id" integer,
-                                         "value" varchar(20) UNIQUE
+CREATE TABLE "event_place" (
+    "event_place_id" SERIAL PRIMARY KEY,
+    "event_id" integer REFERENCES "event" ("event_id") on delete set null on update cascade,
+    "place_id" integer REFERENCES "place" ("place_id") on delete set null on update cascade,
+    "event_date" timestamp NOT NULL,
+    "created_at" timestamp DEFAULT 'now()',
+    "updated_at" timestamp DEFAULT 'now()'
 );
-CREATE TYPE status_place AS ENUM ('free', 'busy');
--- CREATE TABLE "status_place" (
---                                 "status_place_id" integer PRIMARY KEY,
---                                 "value" varchar(20) UNIQUE
--- );
-
-CREATE TABLE "event_places" (
-                                "event_place_id" integer PRIMARY KEY,
-                                "event_id" integer,
-                                "place_id" integer,
-                                "event_date" timestamp,
-                                "created_at" timestamp,
-                                "updated_at" timestamp
-);
-
-ALTER TABLE "reports" ADD FOREIGN KEY ("report_created_by") REFERENCES "user" ("user_id");
-
-ALTER TABLE "task" ADD FOREIGN KEY ("task_user_to") REFERENCES "user" ("user_id");
-
-ALTER TABLE "task" ADD FOREIGN KEY ("task_user_from") REFERENCES "user" ("user_id");
-
-ALTER TABLE "task" ADD FOREIGN KEY ("task_status_id") REFERENCES "status" ("status_id");
-
-ALTER TABLE "test_result" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
-
-ALTER TABLE "test_result" ADD FOREIGN KEY ("fraction_id") REFERENCES "fraction" ("fraction_id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("user_role_id") REFERENCES "role" ("role_id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("user_fraction_id") REFERENCES "fraction" ("fraction_id");
-
-ALTER TABLE "event_user" ADD FOREIGN KEY ("participation_id") REFERENCES "user" ("user_id");
-
-ALTER TABLE "event_user" ADD FOREIGN KEY ("event_id") REFERENCES "event" ("event_id");
-
-ALTER TABLE "places_arendator" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("places_id");
-
-ALTER TABLE "places_arendator" ADD FOREIGN KEY ("arendator_id") REFERENCES "user" ("user_id");
-
-ALTER TABLE "places" ADD FOREIGN KEY ("fraction_id") REFERENCES "fraction" ("fraction_id");
-
--- ALTER TABLE "places" ADD FOREIGN KEY ("status_id") REFERENCES "status_place" ("status_place_id");
-
-ALTER TABLE "places_characteristic" ADD FOREIGN KEY ("characteristic_id") REFERENCES "characteristic" ("charectiristic_id");
-
-ALTER TABLE "places_characteristic" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("places_id");
-
-ALTER TABLE "event_places" ADD FOREIGN KEY ("event_id") REFERENCES "event" ("event_id");
-
-ALTER TABLE "event_places" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("places_id");
---
+++ trigger.sql
@@ -0,0 +1,100 @@
+-- Add fraction
+CREATE OR REPLACE FUNCTION add_fraction_to_user()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE "person" SET "person_fraction_id" = NEW."fraction_id";
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_update_trigger
+AFTER INSERT
+ON "test_result"
+FOR EACH ROW
+EXECUTE FUNCTION add_fraction_to_user();
+
+-- Before adding to places_arendator, check if free
+CREATE OR REPLACE FUNCTION check_before_insert_into_places_arendator()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM places_arendator
+        WHERE (NEW.from_time < "to_time" AND NEW.to_time < "to_time")
+            OR (NEW.from_time > "from_time" AND NEW.to_time > "from_time")
+    ) THEN
+        RAISE EXCEPTION 'The time interval overlaps with existing data';
+        RETURN NULL;
+    ELSE
+        -- If no overlaps, perform the insertion
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_insert_trigger
+BEFORE INSERT ON places_arendator
+FOR EACH ROW
+EXECUTE FUNCTION check_before_insert_into_places_arendator();
+
+-- Role updated_at
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER role_updated_at_trigger
+BEFORE UPDATE ON "role"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Reports updated_at
+CREATE TRIGGER reports_updated_at_trigger
+BEFORE UPDATE ON "report"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- User updated_at
+CREATE TRIGGER user_updated_at_trigger
+BEFORE UPDATE ON "person"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Task updated_at
+CREATE TRIGGER task_updated_at_trigger
+BEFORE UPDATE ON "task"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Fraction updated_at
+CREATE TRIGGER fraction_updated_at_trigger
+BEFORE UPDATE ON "fraction"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Event updated_at
+CREATE TRIGGER event_updated_at_trigger
+BEFORE UPDATE ON "event"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Event_places updated_at
+CREATE TRIGGER event_places_updated_at_trigger
+BEFORE UPDATE ON "event_place"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Places updated_at
+CREATE TRIGGER places_updated_at_trigger
+BEFORE UPDATE ON "place"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- Characteristic updated_at
+CREATE TRIGGER characteristic_updated_at_trigger
+BEFORE UPDATE ON "characteristic"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
