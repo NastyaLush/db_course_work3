a9200ffb15b4d733ab27548d2a1a810751aa19bb
blue <336767@niuitmo.ru>
2024-01-06 10:18:03 +0300

    some fixes

---
+++ main.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Stack;
+
+public class main {
+
+    public static void main(String[] args) {
+        String s = null;
+        s.substring(1,2);
+       int c=2;
+       switch (c){
+           case 2:
+               System.out.println("ss");
+           default:
+       }
+//        int arr[] = { 22, 2, 10 };
+
+
+//        int[] arr = { 1, 11, 3 };
+
+
+
+        int[] arr = new int[] { 33, 41, 0 };
+
+
+    }
+    interface I{
+
+    }
+
+}
