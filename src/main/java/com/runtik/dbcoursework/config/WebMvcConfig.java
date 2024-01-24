a5b43664811c7857f640e8cb284b028bbc704170
Bakhteev <bakhteevb@gmail.com>
2024-01-24 14:44:45 +0300

    added static files

---
+++ src/main/java/com/runtik/dbcoursework/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.runtik.dbcoursework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        WebMvcConfigurer.super.configureViewResolvers(registry);
+//    }
+}
